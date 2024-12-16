899952198885fd7f4fea37b70294198eacef710b
Victor-P-V <pavloff.vik2018@gmail.com>
2024-12-16 13:24:30 +0800

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as a
+from true_math import divide as s
+
+
+result1 = a(69, 3)
+result2 = a(3, 0)
+result3 = s(49, 7)
+result4 = s(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
